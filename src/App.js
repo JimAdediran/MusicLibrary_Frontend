d4c6fa548265646084d1e75edaccf536ff308424
Jimmy Adediran <jimmy@Jimmys-MacBook-Pro.local>
2022-09-13 11:02:31 -0400

    created async function get all songs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import {render, screen} from '@testing-library/react';
 import './App.css';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 function App() {
+
+const [songs, setSongs] = useState([]);
+
+useEffect(() => {
+  getAllSongs();
+}, [])
+
+async function getAllSongs(){
+  const response = await axios.get('http://127.0.0.1:8000/music/');
+  console.log(response.data)
+}
+
   return (
     <div>
       Hello world
