e7f4a3c9eddb1f9f0df93fcdab09b0ec4f47060c
Jimmy Adediran <jimmy@Jimmys-MacBook-Pro.local>
2022-09-12 21:55:39 -0400

    react project initiated

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import {render, screen} from '@testing-library/react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      Hello world
+    </div>
+  );
+}
+
+export default App;
